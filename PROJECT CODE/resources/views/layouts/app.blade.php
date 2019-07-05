b41c01b057aefd5a2af27b111203338f3240ccfd
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-07-06 04:13:21 +0600

    added navbar and some minor changes

--- PROJECT CODE/resources/views/layouts/app.blade.php
+++ PROJECT CODE/resources/views/layouts/app.blade.php
@@ -33,7 +33,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
