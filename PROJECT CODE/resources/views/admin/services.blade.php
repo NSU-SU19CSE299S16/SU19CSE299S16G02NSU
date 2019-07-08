c602f41ed95a79c4857ee4f9a337c7c93f6c91ef
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-08 19:27:18 +0600

    issue:#6

---
+++ PROJECT CODE/resources/views/admin/services.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'HAMDS')}}</title>
+        </head>
+    <body>
+       <h1>SERVICE</h1>
+       <p>This is the services page</p>
+    </body>
+</html>
