dc83dc2cceabb194afc416febbddacee3820d115
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-07-10 04:44:13 +0600

    the medicine index page edited, issue #6

--- PROJECT CODE/resources/views/medicine/medicines.blade.php
+++ PROJECT CODE/resources/views/medicine/medicines.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<h4 class="text-center lead">This is the list of all the medicine that are available in our inventory.</h4>
+<div class="container">
+    <h4 class="text-center lead">This is the list of all the medicine that are available in our inventory.</h4>
 
+    <div class="list-group mt-5">
 
+        <div class="row justify-content-center">
+            <div class="col-md-2 text-center">
+                @foreach($medicines as $medicine)
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-info">{{$medicine->med_name}}</a>
+                @endforeach
+            </div>
+        </div>
 
-
+    </div>
+</div>
 @endsection
