cf984bc0b9a2a5b52e8268c1577e6dda3ea83230
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-17 00:17:43 +0600

    issue #14 : all the database added

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -11,7 +11,10 @@
                 <th scope="row">{{$doctor->doc_id}}</th>
                 <td>{{$doctor->doc_name}}</td>
                 <td>{{$doctor->doc_spec}}</td> 
-                <td>{{$doctor->doc_hospital}}</td> 
+                <td>{{$doctor->doc_hospital}}</td>
+                <td>{{$doctor->med_contact}}</td> 
+                <td>{{$doctor->created_at}}</td>
+                <td>{{$doctor->updated_at}}</td>
                 </tr>  
             @endforeach
         </tbody>
