b3c56b819647ecda5b441757e2c3100376e096bd
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-24 09:43:12 +0600

    Enhancement #14 : single view edited

--- PROJECT CODE/resources/views/doctor/doctor.blade.php
+++ PROJECT CODE/resources/views/doctor/doctor.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <div class="container">
     <h4 class="text-center lead">Doctor Information</h4>
+    
+
+        <div class="row justify-content-center">
+            <div class="col-md-2 text-center">
+                <a href="" class="list-group-item list-group-item-action list-group-item-success">{{$doctor->doc_name}}</a>
+                
+            </div>
+
+            
+        </div>
+
 
     <div class="list-group mt-5">
     </div>
