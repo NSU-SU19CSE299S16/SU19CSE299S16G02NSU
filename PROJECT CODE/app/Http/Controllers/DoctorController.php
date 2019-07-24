8c0cbc83c63a2a7a8bc30fcb2cb533b4ced22902
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-24 09:31:29 +0600

    Enhancement #14 : database links

--- PROJECT CODE/app/Http/Controllers/DoctorController.php
+++ PROJECT CODE/app/Http/Controllers/DoctorController.php
@@ -6,10 +6,7 @@ use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    public function index(){
-        return view('admin.index');
-    }
-
+    
     
     public function about(){
         return view('admin.about');
@@ -18,4 +15,29 @@ class DoctorController extends Controller
     public function services(){
         return view('admin.services');
     }
+
+    //show all doctors
+    public function index(){
+        $doctors = DB::table('doctors')->get();
+        return view('doctor.doctors', ['doctor' => $doctors]);
+    
+    }
+
+    //show a specific doctor
+    public function show($id){
+        $doctor = DB::table('doctors')->where('doc_id', $id)->first();
+        return view('doctor.doctors', ['doctor' => $doctor]);
+    
+    }
+    
+    //show all doctor (admin panel)
+    public function adminIndex(){    
+        $doctors = DB::table('doctors')->get();
+        return view('admin.doctor.doctors', ['doctors' => $doctors]);
+    
+    }
+
+    
 }
+
+
---
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h4 class="text-center lead">All Doctors</h4>
+
+    <table class="table table-hover table-info"> 
+     <thead>
+            <tr class="bg-info">
+            <th scope="col">#</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            </tr>
+        </thead>
+              
+        <tbody>
+            @foreach($doctors as $doctor)
+             <tr>
+                <th scope="row">{{$doctor->doc_id}}</th>
+                <td>{{$doctor->doc_name}}</td>
+                <td>{{$doctor->doc_spec}}</td> 
+                <td>{{$doctor->doc_hospital}}</td>
+                <td>{{$doctor->doc_contact}}</td> 
+                <td>{{$doctor->created_at}}</td>
+                <td>{{$doctor->updated_at}}</td>
+                </tr>  
+                <td><button class="btn btn-danger">DELETE</button></td>
+                <td><button class="btn btn-info">UPDATE</button></td>
+               
+            @endforeach
+        </tbody>
+    </table>
+
+    
+</div>
+@endsection
