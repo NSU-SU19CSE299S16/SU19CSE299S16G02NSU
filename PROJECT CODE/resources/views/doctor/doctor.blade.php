5079ec07b674ea1217aeee1e9c18f2670a36a955
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-24 09:38:37 +0600

    Enhancement #14 : single doctor view created

---
+++ PROJECT CODE/resources/views/doctor/doctor.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h4 class="text-center lead">Doctor Information</h4>
+
+    <div class="list-group mt-5">
+    </div>
+</div>
+@endsection
