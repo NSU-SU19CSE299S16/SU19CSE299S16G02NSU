3685c909d9772477ce93e343ae13d61635e8ad52
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-30 23:31:14 +0600

    Enhancement #10 :route to update

--- PROJECT CODE/resources/views/admin/doctor/edit.blade.php
+++ PROJECT CODE/resources/views/admin/doctor/edit.blade.php
@@ -1,5 +1,9 @@
 @section('content')
 <div class="container">
     <h4 class="text-center lead">Edit a Doctor</h4>
-    </div>
+    <form method="POST" action="{{route('doctor.update', ['id' => $doctor->doc_id])}}">
+    
+
+</div>
+
 @endsection
