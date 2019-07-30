eb259b0dd86f6622755239725ff05bb1ec3c4957
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-31 00:52:35 +0600

    Enhancement #14 :added references to all doctor view

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -3,5 +3,9 @@
 @section('content')
 <div class="container">
     <h4 class="text-center lead">This is the list of all the Doctors that are available in our inventory.</h4>
-
+            @foreach($doctors as $doctor)
+                    <a href="{{route('doctor.show', ['id' => $doctor->dpc_id])}}" class="list-group-item list-group-item-action list-group-item-info">{{$doctor->doc_name}}</a>
+                @endforeach
+        
+       
 @endsection
