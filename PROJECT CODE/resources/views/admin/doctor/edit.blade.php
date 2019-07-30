59f729fb00db5b7053333383c61156c6aabe0ffb
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-30 23:32:02 +0600

    Enhancement #10 : Minor changes in edit

--- PROJECT CODE/resources/views/admin/doctor/edit.blade.php
+++ PROJECT CODE/resources/views/admin/doctor/edit.blade.php
@@ -2,7 +2,13 @@
 <div class="container">
     <h4 class="text-center lead">Edit a Doctor</h4>
     <form method="POST" action="{{route('doctor.update', ['id' => $doctor->doc_id])}}">
-    
+     {{ method_field('PUT') }}
+    @csrf
+        <div class="form-row">
+            <div class="col">
+            <input type="text" name="name" class="form-control" placeholder="{{$doctor->doc_name}}">
+            </div>
+   
 
 </div>
 
