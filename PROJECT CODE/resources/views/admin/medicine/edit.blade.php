db32c481124576e8d02815824aff7981329594f5
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-07-30 06:17:20 +0600

    Enhancement  #10 : Updated the form for edit

---
+++ PROJECT CODE/resources/views/admin/medicine/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h4 class="text-center lead">Add a new medicine</h4>
+    <form method="POST" action="{{route('medicine.store')}}">
+    @csrf
+        <div class="form-row">
+            <div class="col">
+            <input type="text" name="name" class="form-control" placeholder="{{$medicine->med_name}}">
+            </div>
+            <div class="col">
+            <input type="text" name="stock" class="form-control" placeholder="{{$medicine->med_stock}}">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary my-3">Add</button>
+    </form>
+</div>
+@endsection
