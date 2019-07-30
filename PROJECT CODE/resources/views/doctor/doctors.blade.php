f10a62c91897282463bc046b0ff9c99d38e3f14e
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-31 00:49:44 +0600

    Enhancement #14 : All doctor view updated

---
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h4 class="text-center lead">This is the list of all the Doctors that are available in our inventory.</h4>
+
+@endsection
