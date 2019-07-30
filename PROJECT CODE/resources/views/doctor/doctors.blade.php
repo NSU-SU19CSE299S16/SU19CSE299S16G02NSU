5f6566a6e06f8770350536420dcf8dd168d73226
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-31 00:55:09 +0600

    Enhancement #14 : added some styles to the view all doctors

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -3,9 +3,20 @@
 @section('content')
 <div class="container">
     <h4 class="text-center lead">This is the list of all the Doctors that are available in our inventory.</h4>
+            <div class="list-group mt-5">
+
+        <div class="row justify-content-center">
+            <div class="col-md-2 text-center">
+                
             @foreach($doctors as $doctor)
                     <a href="{{route('doctor.show', ['id' => $doctor->doc_id])}}" class="list-group-item list-group-item-action list-group-item-info">{{$doctor->doc_name}}</a>
                 @endforeach
+                </div>
+        </div>
+
+    </div>
+</div>
+
         
        
 @endsection
