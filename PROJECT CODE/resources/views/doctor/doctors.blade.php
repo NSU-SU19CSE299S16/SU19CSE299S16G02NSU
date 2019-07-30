67a606808846a7bb707e26e1548e1eaf6cff0522
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-31 00:53:27 +0600

    Enhancement #14 :minor changes in all view

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <h4 class="text-center lead">This is the list of all the Doctors that are available in our inventory.</h4>
             @foreach($doctors as $doctor)
-                    <a href="{{route('doctor.show', ['id' => $doctor->dpc_id])}}" class="list-group-item list-group-item-action list-group-item-info">{{$doctor->doc_name}}</a>
+                    <a href="{{route('doctor.show', ['id' => $doctor->doc_id])}}" class="list-group-item list-group-item-action list-group-item-info">{{$doctor->doc_name}}</a>
                 @endforeach
         
        
