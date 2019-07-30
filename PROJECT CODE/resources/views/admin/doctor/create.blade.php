410d81cb6b58e2be75d82070548c52cc269722e9
Sidratul Muntaha <smuntaha97@yahoo.com>
2019-07-30 23:54:46 +0600

    Enhancement #10 : Doctorname added in create

--- PROJECT CODE/resources/views/admin/doctor/create.blade.php
+++ PROJECT CODE/resources/views/admin/doctor/create.blade.php
@@ -3,7 +3,14 @@
 @section('content')
 <div class="container">
     <h4 class="text-center lead">Add a new Doctor</h4>
+    <form method="POST" action="{{route('doctor.store')}}">
+    @csrf
+        <div class="form-row">
+            <div class="col">
+            <input type="text" name="name" class="form-control" placeholder="Doctor Name">
+            </div>
     
+
     
    
 </div>
