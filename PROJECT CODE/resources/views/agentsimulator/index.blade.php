1a552556504407d38e1d49f015ef504d90d99f2f
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-04 02:39:45 +0600

    Enhancement #27 : added send-money form

--- PROJECT CODE/resources/views/agentsimulator/index.blade.php
+++ PROJECT CODE/resources/views/agentsimulator/index.blade.php
@@ -1,3 +1,22 @@
 @extends('layouts.app')
-
 @section('content')
+
+
+<div class="container">
+
+<div class="agent d-flex justify-content-center align-items-center">
+
+
+<form>
+  <div class="form-group">
+    <input type="email" class="form-control" id="amount" aria-describedby="amount" placeholder="Enter Amount">
+    <small id="amount" class="form-text text-muted">Please Enter The Amount</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Send Money</button>
+</form>
+
+</div>
+
+</div>
+
+@endsection
