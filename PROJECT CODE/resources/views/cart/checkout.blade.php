0822f46846cb63d8ae9d4d5e813871c036cc1840
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-07 05:46:51 +0600

    Enhancement #30 : Added bkash and ucash logos to page

--- PROJECT CODE/resources/views/cart/checkout.blade.php
+++ PROJECT CODE/resources/views/cart/checkout.blade.php
@@ -4,24 +4,15 @@
 <div class="container">
     <form action="{{route('Order.store')}}" method="POST">
     @csrf
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="pay_method" id="bkash" value="bkash" checked>
-        <label class="form-check-label" for="bkash">
-            bKash
+
+        <label class="pay_method">
+            <input type="radio" name="pay_method" value="bkash" >
+            <img src="{{asset('assets/bkash-logo.jpg')}}">        
         </label>
-        </div>
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="pay_method" id="rocket" value="rocket">
-        <label class="form-check-label" for="rocket">
-            Rocket
+        <label class="pay_method">
+            <input type="radio" name="pay_method" value="ucash" >
+            <img src="{{asset('assets/ucash-logo.png')}}">        
         </label>
-        </div>
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="pay_method" id="ucash" value="ucash">
-        <label class="form-check-label" for="ucash">
-            Ucash
-        </label>
-    </div>
         <button type="submit" class="crt-btn">Place Order</button>
     </form>
 
