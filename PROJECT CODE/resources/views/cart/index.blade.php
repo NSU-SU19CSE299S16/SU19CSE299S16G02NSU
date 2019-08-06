a5b5300ed3c953622da01e9448c3a9088811e2a4
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-07 03:54:21 +0600

    Enhancement #30 : Styled the cart table

--- PROJECT CODE/resources/views/cart/index.blade.php
+++ PROJECT CODE/resources/views/cart/index.blade.php
@@ -2,53 +2,58 @@
 
 @section('content')
 <div class="container">
-	<table>
-		<thead>
-			<tr>
-				<th>Product</th>
-				<th>Qty</th>
-				<th>Price</th>
-				<th>Subtotal</th>
-			</tr>
-		</thead>
+	<div class="row">
+		<div class="col-md-7">
+			<table class="table table-hover table-dark" >
+				<thead>
+				<tr class="">
+					<th scope="col">Product</th>
+					<th scope="col">Quantity</th>
+					<th scope="col">Price</th>
+					<th scope="col">Subtotal</th>
+					</tr>
+				</thead>
+				<tbody>
 
-		<tbody>
+			@foreach(Cart::content() as $row)
 
-			<?php foreach(Cart::content() as $row) :?>
 
-				<tr>
-					<td>
-						<p><strong><?php echo $row->name; ?></strong></p>
-						<p><?php echo ($row->options->has('size') ? $row->options->size : ''); ?></p>
-					</td>
-					<td><input type="text" value="<?php echo $row->qty; ?>"></td>
-					<td>$<?php echo $row->price; ?></td>
-					<td>$<?php echo $row->total; ?></td>
-				</tr>
+					<tr class="">
+					<th>						
+						<p>{{$row->name}}</p>
+						<p>{{($row->options->has('size') ? $row->options->size : '')}}</p>
+					</th>
+					<td>{{$row->qty}}</td>
+					<td>{{$row->price}}</td>
+					<td>{{$row->subtotal}}</td>
+					</tr>
 
-			<?php endforeach;?>
 
-		</tbody>
-		
-		<tfoot>
-			<tr>
-				<td colspan="2">&nbsp;</td>
-				<td>Subtotal</td>
-				<td><?php echo Cart::subtotal(); ?></td>
-			</tr>
-			<tr>
-				<td colspan="2">&nbsp;</td>
-				<td>Tax</td>
-				<td><?php echo Cart::tax(); ?></td>
-			</tr>
-			<tr>
-				<td colspan="2">&nbsp;</td>
-				<td>Total</td>
-				<td><?php echo Cart::total(); ?></td>
-			</tr>
-		</tfoot>
-	</table>
+			@endforeach
 
+
+			</tbody>
+			
+				<tfoot>
+					<tr>
+						<td colspan="2">&nbsp;</td>
+						<td>Subtotal</td>
+						<td><?php echo Cart::subtotal(); ?></td>
+					</tr>
+					<tr>
+						<td colspan="2">&nbsp;</td>
+						<td>Tax</td>
+						<td><?php echo Cart::tax(); ?></td>
+					</tr>
+					<tr>
+						<td colspan="2">&nbsp;</td>
+						<td>Total</td>
+						<td><?php echo Cart::total(); ?></td>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
+	</div>
 	<form action="{{route('cart.checkout')}}" method="GET">
         <button type="submit" class="btn btn-warning">Proceed To Checkout</button>
     </form>
