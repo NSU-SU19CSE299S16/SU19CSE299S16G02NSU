a0bdbbc714803b5acbc32f49f271aec98f4dc379
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-08 04:54:51 +0600

    Enhancement #33 : Added image to user table

--- PROJECT CODE/database/migrations/2014_10_12_000000_create_users_table.php
+++ PROJECT CODE/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('filename')->nullable();
+            $table->string('mime')->nullable();
+            $table->string('original_filename')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
