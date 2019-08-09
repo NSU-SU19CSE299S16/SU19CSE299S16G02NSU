b52d0a68c8ec24b1a70bc6df8a7dc1a7d2ef1fa4
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-09 06:41:58 +0600

    Enhancement #31 : added icons

--- PROJECT CODE/resources/views/admin/dashboard.blade.php
+++ PROJECT CODE/resources/views/admin/dashboard.blade.php
@@ -7,28 +7,28 @@
         <div class="admin-count col-md-3">
             <div class="card">
                 <div class="card-body">
-
+                <i class="fas fa-capsules"></i>{{$med_count}}
                 </div>
             </div>
         </div>
         <div class="admin-count col-md-3">
             <div class="card">
                 <div class="card-body">
-                    
+                <i class="fas fa-user-md"></i>{{$doc_count}}
                 </div>
             </div>
         </div>
         <div class="admin-count col-md-3">
             <div class="card">
                 <div class="card-body">
-                    
+                <i class="fas fa-chart-line"></i>{{$order_count}}
                 </div>
             </div>
         </div>
         <div class="admin-count col-md-3">
             <div class="card">
                 <div class="card-body">
-                    
+                <i class="fas fa-users"></i>{{$user_count}}
                 </div>
             </div>
         </div>
