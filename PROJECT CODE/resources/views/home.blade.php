b688d882dbdc52175ccd1df035a6f9f09274c3e8
Arindam Kabir <aarindam.ahmed@northosuth.edu>
2019-08-16 14:58:59 +0600

    Enhancement #30 : Added styling to the card

--- PROJECT CODE/resources/views/home.blade.php
+++ PROJECT CODE/resources/views/home.blade.php
@@ -31,54 +31,20 @@
         </div>
     </div>
 
-    @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Search Bar -->
-
-    <div class="row justify-content-center text-center my-4">
-        <div class="col-md-9">
-            <form>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
-                <button type="submit" class="btn btn-outline-dark my-2">Search</button>   
-            </form>
-        </div>
-    </div>
-
     <h3>Featured Medicines</h3>
       
         <div class="row">
 
-        
+
       
             <div class="col-md-3">
 
-                foreach($medicines as $medicine)
 
                 <div class="card-link">
                     <a href="#">
                         <div class="card my-3">
                             <div class="card-img">
-                                <img class="card-img-top" src="{{asset('assets/zimax.jpg')}}" alt="Card image cap">
+                                <img class="card-img-top" src="{{asset('assets/entacyd.jpg')}}" alt="Card image cap">
                                 <div class="med-details">500mg Tablet</div>
                             </div>
                             <div class="card-body">
@@ -95,10 +61,8 @@
 
 
 
-</div>
-@endsection
-
-
 
 </div>
 @endsection
+
+
