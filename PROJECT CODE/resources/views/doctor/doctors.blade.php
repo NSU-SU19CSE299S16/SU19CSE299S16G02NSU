6d8a92aa235bbf00cea65054dc8ef7ed2f5f1bcb
smuntaha97 <sidratul.muntaha02@northsouth.edu>
2019-08-17 14:10:56 +0600

    Bug #39 : cleaned the doctors view

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -2,37 +2,8 @@
 
 @section('content')
 <div class="container">
-    <h4 class="text-center lead">All Doctors</h4>
 
-    <table class="table table-hover table-info"> <thead>
-            <tr class="bg-info">
-            <th scope="col">#</th>
-            <th scope="col">Doctor Name</th>
-            <th scope="col">Specialized In</th>
-            <th scope="col">Hospital</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            </tr>
-        </thead>
-       
-        <tbody>
-            @foreach($doctors as $doctor)
-             <tr>
-                <th scope="row">{{$doctor->doc_id}}</th>
-                <td>{{$doctor->doc_name}}</td>
-                <td>{{$doctor->doc_spec}}</td> 
-                <td>{{$doctor->doc_hospital}}</td>
-                <td>{{$doctor->doc_contact}}</td> 
-                <td>{{$doctor->created_at}}</td>
-                <td>{{$doctor->updated_at}}</td>
-                </tr>  
-                <td><button class="btn btn-danger">DELETE</button></td>
-                <td><button class="btn btn-info">UPDATE</button></td>
-               
-            @endforeach
-        </tbody>
-    </table>
+
 
     
 </div>
