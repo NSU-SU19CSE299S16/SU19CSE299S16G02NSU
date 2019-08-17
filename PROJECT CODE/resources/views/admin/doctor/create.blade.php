afb26ae89a840b2e43f5939950ea283d3b10342b
smuntaha97 <sidratul.muntaha02@northsouth.edu>
2019-08-17 13:49:21 +0600

    Bug #39 : updated the add doctor page

--- PROJECT CODE/resources/views/admin/doctor/create.blade.php
+++ PROJECT CODE/resources/views/admin/doctor/create.blade.php
@@ -9,11 +9,17 @@
             <div class="col">
             <input type="text" name="name" class="form-control" placeholder="Doctor Name">
             </div>
-            <div class="col">
-            <input type="text" name="spec" class="form-control" placeholder="Specialized in">
-            </div>
         </div>
-    <button type="submit" class="btn btn-primary my-3">Add</button>
+        <div class="col">
+            <input type="text" name="spec" class="form-control" placeholder="Specialized in">
+        </div>
+        <div class="col">
+            <input type="text" name="hospital" class="form-control" placeholder="Doctor Hospital">
+        </div>
+        <div class="col">
+            <input type="text" name="contact" class="form-control" placeholder="Contact">
+        </div>
+    <button type="submit" class="btn btn-primary my-3">Add Doctor</button>
     </form>
 </div>
 @endsection
