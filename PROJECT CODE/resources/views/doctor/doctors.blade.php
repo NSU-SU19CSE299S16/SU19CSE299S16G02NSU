ebf6809acd6cbd0403174098f7893d805635d330
arindamkabir <aarindam.ahmed@northsouth.edu>
2019-08-17 16:17:31 +0600

    Enhancement #41: showing doctors list in apointment page

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -3,7 +3,16 @@
 @section('content')
 <div class="container">
 
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action">Doctors</a>
 
+  @foreach($doctors as $doctor)
+
+  
+  <a href="#" class="list-group-item list-group-item-action list-group-item-info">{{$doctor->doc_name}}</a>
+  @endforeach
+
+</div>
 
     
 </div>
