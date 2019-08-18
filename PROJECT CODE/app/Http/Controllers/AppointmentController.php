da0b107495496deda4016b6649e5758f329ca446
arindamkabir <aarindam.ahmed@northsouth.edu>
2019-08-18 06:33:56 +0600

    Enhancement #41: added store function in controller

--- PROJECT CODE/app/Http/Controllers/AppointmentController.php
+++ PROJECT CODE/app/Http/Controllers/AppointmentController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class AppointmentController extends Controller
 {
-    //
+    public function store(Request $request){
+
+    $data = DB::table('appointments')->insert(
+        [ 
+            'user_id' => \Auth::id(),
+            'pay_method' =>  $request->pay_method,
+            'total' => Cart::total(),
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+
+        return redirect()->route('doctor.doctors')->with('success', 'Appointment Successfully Requested. You will be notified after confirmation.');
+
+    }
+   
 }
