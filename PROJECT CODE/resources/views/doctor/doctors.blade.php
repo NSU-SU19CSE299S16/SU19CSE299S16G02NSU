6913a8160a4a254aac72aca0541c643706a95e28
smuntaha97 <sidratul.muntaha02@northsouth.edu>
2019-08-20 11:36:23 +0600

    Enhancment #43 : Styled the doctors page

--- PROJECT CODE/resources/views/doctor/doctors.blade.php
+++ PROJECT CODE/resources/views/doctor/doctors.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<!-- Toast -->
+<div class="container mt-3">
 @if (session('success'))
 
 <div class="alert alert-success my-3" role="alert">
@@ -10,15 +9,45 @@
 </div>
 
 @endif
+<h3>Doctors</h3>
 
 <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action">Doctors</a>
 
   @foreach($doctors as $doctor)
 
   
-  <a href="#" class="list-group-item list-group-item-action list-group-item-info">
-  <p>{{$doctor->doc_name}}</p>
+  <a href="#" class="list-group-item list-group-item-action my-2">
+    <div class="row">
+      <div class="col-md-4 doc-image">
+
+      </div>
+      <div class="col-md-8 doc-details">
+      <dl class="row">
+        <dt class="col-sm-3">Description lists</dt>
+        <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
+
+        <dt class="col-sm-3">Euismod</dt>
+        <dd class="col-sm-9">
+          <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+          <p>Donec id elit non mi porta gravida at eget metus.</p>
+        </dd>
+
+        <dt class="col-sm-3">Malesuada porta</dt>
+        <dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+
+        <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
+        <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+
+        <dt class="col-sm-3">Nesting</dt>
+        <dd class="col-sm-9">
+          <dl class="row">
+            <dt class="col-sm-4">Nested definition list</dt>
+            <dd class="col-sm-8">Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc.</dd>
+          </dl>
+        </dd>
+      </dl>
+      </div>
+    </div>
   <form action="{{route('appointment.store')}}" method="POST">
     @csrf
         <input type="hidden" name="doctor_id" class="form-control" value="{{$doctor->doc_id}}">
