25d46119b39d7d1cdbdefc670ae3365cab97f1f5
arindamkabir <aarindam.ahmed@northsouth.edu>
2019-08-21 07:43:56 +0600

    Enhancement #30 : finished styling home image

--- PROJECT CODE/resources/views/home.blade.php
+++ PROJECT CODE/resources/views/home.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="home-section">
+    <div class="container">
+        <div class="homepage-text">
+        <h1 class="text-left display-3">Welcome To HAMDS<h1>
+        <h5 class="lead text-left">The largest online healthcare website in Bangladesh</h5>
+        <div class="text-left mt-4">
+            <button class="mr-2"><a class="" href="{{route('medicine.index')}}">Medicines</a></button>
+            <button class=""><a class="" href="{{route('doctor.index')}}">Make Appointment</a></button>
+        </div>
+
+        </div>
+    </div>
+</div>
 <div class="container">
 
     <!-- Search Bar -->
 
-    <div class="row justify-content-center text-center my-4">
+
+    <!-- <div class="row justify-content-center text-center my-4">
         <div class="col-md-9">
             <form>
             <div class="input-group mb-3">
@@ -16,7 +30,7 @@
             </div>
             </form>
         </div>
-    </div>
+    </div> -->
 
     <h3>Featured Medicines</h3>
       
